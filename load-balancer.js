447f0d4730cc7a2985d6e65776a91e0d1888984b
mathiasrotheisen <matrote@hotmail.com>
2020-09-27 20:12:23 +0200

    made actual assignment

--- load-balancer.js
+++ load-balancer.js
@@ -1,25 +1,20 @@
-var http = require('http')
-, httpProxy = require('http-proxy')
-, seaport = require('seaport')
-, ports = seaport.connect('localhost', 9090);
+// require('http')
+// require('http-proxy')
+// require('seaport')
+// seaport.connect('localhost', 9090);
 
-var i = -1;
+// Reference til step 4
+// var i = - 1
 
-var proxy = httpProxy.createProxyServer({});
-var server = http.createServer(function(req, res) {
-	var addresses = ports.query('add-server');
-	if (!addresses.length) {
-        res.writeHead(503, { 'Content-Type': 'text/plain' });
-        res.end('Service unavailable');
-        return;
-    }
-	i = (i + 1) % addresses.length;
-	var host = addresses[i].host.split(":").reverse()[0];
-	var port = addresses[i].port;
-	proxy.web(req, res, { target: 'http://' + host + ':' + port });	
-});
+// httpProxy.createProxyServer({});
 
-server.listen(8000, function () {
-	console.log('load balancer listening on port %d', 8000);
-});
+// var server = http.createServer
+
+// Reference til step 4
+/* i = (i + 1) % addresses.length;
+var host = addresses[i].host.split(":").reverse()[0];
+var port = addresses[i].port;
+proxy.web(req, res, { target: 'http://' + host + ':' + port });	
+*/ 
+
 
---
+++ server.js
@@ -0,0 +1,9 @@
+// require('http')
+// require('seaport')
+// seaport.connect('localhost', 9090);
+
+// Her skal der være noget funktionalitet
+
+//var server = http.createServer(function (req, res)
+
+//server.listen(ports.register('server')
