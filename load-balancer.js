c5697477df7c758685d0ca72c5a02b7e8bd8e086
Morten Laursen <mortendlaursen@gmail.com>
2020-11-25 19:33:34 +0100

    solution

--- load-balancer.js
+++ load-balancer.js
@@ -1,20 +1,31 @@
-// require('http')
-// require('http-proxy')
-// require('seaport')
-// seaport.connect('localhost', 9090);
+// step 1
+var http = require("http"),
+  httpProxy = require("http-proxy"),
+  seaport = require("seaport"),
+  ports = seaport.connect("localhost", 9090),
+  path = require("path"),
+  fs = require("fs");
 
 // Reference til step 4
-// var i = - 1
+var i = -1;
 
-// httpProxy.createProxyServer({});
+// step 2:
+let proxy = httpProxy.createProxyServer({});
 
-// var server = http.createServer
+// step 3:
+var server = http.createServer(function (req, res) {
+  let addresses = ports.query("server");
+  if (addresses.length == 0) res.end("No servers available");
+  else {
+    i = (i + 1) % addresses.length;
+    var host = addresses[i].host.split(":").reverse()[0];
+    var port = addresses[i].port;
+    proxy.web(req, res, {
+      target: "http://" + host + ":" + port
+    });
+  }
+});
 
-// Reference til step 4
-/* i = (i + 1) % addresses.length;
-var host = addresses[i].host.split(":").reverse()[0];
-var port = addresses[i].port;
-proxy.web(req, res, { target: 'http://' + host + ':' + port });	
-*/ 
-
-
+server.listen(8080, function () {
+  console.log("load balancer listening on port ", this.address().port);
+});
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-// require('http')
-// require('seaport')
-// seaport.connect('localhost', 9090);
+//step 1
+var http = require("http");
+// https://github.com/substack/seaport
+var seaport = require("seaport");
+var port = seaport.connect("localhost", 9090);
+const path = require("path");
+const fs = require("fs");
 
 // Her skal der være noget funktionalitet
+// step 2
+function simulation(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
-//var server = http.createServer(function (req, res)
+// step 3
+//https://nodejs.org/api/http.html#http_http_createserver_options_requestlistener
+// returns a net server: https://nodejs.org/api/net.html#net_class_net_server
+var server = http.createServer(function (req, res) {
+  //add headers?
+  // note that this.address() is available because http.createServer
+  // returns a net server.
+  res.end("sum: " + simulation(100_000) + ", port: " + this.address().port);
+});
 
-//server.listen(ports.register('server')
+// step 4
+server.listen(port.register("server"), function () {
+  console.log("server listening on port ", this.address().port);
+});
